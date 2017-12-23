996c8791d9c1ac8145349186ff42380421f3d34d
Allan Wazacz <cazwazacz@gmail.com>
2017-12-23 12:59:38 +0000

    Add text box and send button, tie to ws listener

--- public/script.js
+++ public/script.js
@@ -1,3 +1,16 @@
 (function() {
-  ws = new WebSocket(`ws://${location.host}`);
+  var ws = new WebSocket(`ws://${location.host}`);
+  var messagesDiv = document.getElementById('the-messages');
+  var sendButton = document.getElementById('the-button');
+  var textBox = document.getElementById('text-box');
+
+  ws.onmessage = function(event) {
+    messagesDiv.innerHTML += `<li>${event.data}</li>`;
+  };
+
+  ws.onopen = function() {
+    sendButton.addEventListener('click', function() {
+      ws.send(textBox.value);
+    })
+  };
 })();

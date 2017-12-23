4e597a5c55f7cc9a81407df6afb3fa25cbba9bd0
Allan Wazacz <cazwazacz@gmail.com>
2017-12-23 13:25:40 +0000

    Add name and message to message info + minimal styling

--- public/script.js
+++ public/script.js
@@ -3,14 +3,17 @@
   var messagesDiv = document.getElementById('the-messages');
   var sendButton = document.getElementById('the-button');
   var textBox = document.getElementById('text-box');
+  var nameBox = document.getElementById('name-box');
 
   ws.onmessage = function(event) {
-    messagesDiv.innerHTML += `<li>${event.data}</li>`;
+    message = JSON.parse(event.data);
+    messagesDiv.innerHTML += `<li><strong>${message.name}</strong>: ${message.message}</li>`;
   };
 
   ws.onopen = function() {
     sendButton.addEventListener('click', function() {
-      ws.send(textBox.value);
+      ws.send(JSON.stringify({name: nameBox.value, message: textBox.value}));
+      textBox.value = '';
     })
   };
 })();

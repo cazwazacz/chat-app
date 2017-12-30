fa100744f8b3a6becb56cb750e7261b217a80133
Allan Wazacz <cazwazacz@gmail.com>
2017-12-30 14:32:45 +0000

    Add mongoose and connect to development database

--- index.js
+++ index.js
@@ -2,31 +2,43 @@ const express = require('express');
 const http = require('http');
 const url = require('url');
 const WebSocket = require('ws');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static('public'));
+let databaseUrl = 'mongodb://localhost/chat_development';
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+mongoose.connect(databaseUrl, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Successfully connected to database');
 
-wss.on('connection', function connection(ws, req) {
-  const location = url.parse(req.url, true);
+    app.use(express.static('public'));
 
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
+    const server = http.createServer(app);
+    const wss = new WebSocket.Server({ server });
+
+    wss.on('connection', function connection(ws, req) {
+      const location = url.parse(req.url, true);
+
+      ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+          }
+        });
+      })
+
+      ws.on('error', function(error) {
+        console.log('one client down');
+      })
     });
-  })
 
-  ws.on('error', function(error) {
-    console.log('one client down');
-  })
-});
-
-server.listen(PORT, function listening() {
-  console.log('Listening on %d', server.address().port);
+    server.listen(PORT, function listening() {
+      console.log('Listening on %d', server.address().port);
+    });
+    
+  }
 });

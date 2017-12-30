af46442c12034c0200711730bce91e430585db0a
Allan Wazacz <cazwazacz@gmail.com>
2017-12-30 16:24:49 +0000

    Fetch all messages from api

--- public/script.js
+++ public/script.js
@@ -5,9 +5,23 @@
   var textBox = document.getElementById('text-box');
   var nameBox = document.getElementById('name-box');
 
+  var xhttp = new XMLHttpRequest();
+
   function createMessageDiv(name, message) {
     return `<div><strong>${name}</strong>: ${message}</div>`;
   }
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      JSON.parse(this.response).forEach(function(message) {
+        messagesDiv.innerHTML += createMessageDiv(message.name, message.body);
+      })
+    }
+  }
+
+  xhttp.open("GET", "/api/messages", true);
+  xhttp.send();
+
 
   ws.onmessage = function(event) {
     message = JSON.parse(event.data);
